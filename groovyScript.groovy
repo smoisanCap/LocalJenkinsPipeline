4991f0963a84326338d486919110ad8d1d26e794
Simon Moisan <simon.moisan@capgemini.com>
2019-07-26 13:41:24 +0200

    update groovy script

--- groovyScript.groovy
+++ groovyScript.groovy
@@ -1,18 +1,26 @@
 #!/usr/bin/env groovy
 
 import hudson.model.*
-import hudson.AbortException
-import hudson.console.HyperlinkNote
-import java.util.concurrent.CancellationException
-import jenkins.model.*
- 
-// Retrieve parameters of the current build
-println "Get and print jenkins parameters"
  
 // get current thread / Executor
 def thr = Thread.currentThread()
 // get current build
 def build = thr?.executable
-
-def listOfName = build.buildVariableResolver.resolve("listOfName")
-println "listOfName=$listOfName"
+ 
+ 
+// get parameters
+def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
+parameters.each {
+   println "parameter ${it.name}:"
+   println it.dump()
+   println "-" * 80
+}
+ 
+ 
+// ... or if you want the parameter by name ...
+def hardcoded_param = "listOfName"
+def resolver = build.buildVariableResolver
+def hardcoded_param_value = resolver.resolve(hardcoded_param)
+ 
+ 
+println "param ${hardcoded_param} value : ${hardcoded_param_value}"
