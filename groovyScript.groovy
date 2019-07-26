2dca2a50ef2e47201d601cd7fd7ab6839b0a5977
Simon Moisan <simon.moisan@capgemini.com>
2019-07-26 13:35:34 +0200

    update groovy script

--- groovyScript.groovy
+++ groovyScript.groovy
@@ -8,6 +8,11 @@ import jenkins.model.*
  
 // Retrieve parameters of the current build
 println "Get and print jenkins parameters"
+ 
+// get current thread / Executor
+def thr = Thread.currentThread()
+// get current build
+def build = thr?.executable
 
 def listOfName = build.buildVariableResolver.resolve("listOfName")
 println "listOfName=$listOfName"
